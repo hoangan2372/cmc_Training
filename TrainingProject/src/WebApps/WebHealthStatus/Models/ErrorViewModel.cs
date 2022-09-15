5831104819623473be57094c52859bf537d76f80
nhan1 <nhan1@HCMDW-NHAN1>
2022-09-15 13:41:38 +0700

    cm micro-service solution architect

---
+++ TrainingProject/src/WebApps/WebHealthStatus/Models/ErrorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace WebHealthStatus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorViewModel> _logger;
+
+        public ErrorViewModel(ILogger<ErrorViewModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            /*RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;*/
+        }
+    }
+}
